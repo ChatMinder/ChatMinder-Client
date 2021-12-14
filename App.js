86120c5023c4fad10f1b78da4f93f5dfb66d9173
SEEWON <andyaaa78@gmail.com>
2021-12-14 18:09:34 +0900

    [Chore] Add React Navigation settings

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
