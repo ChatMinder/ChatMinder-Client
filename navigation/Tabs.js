313d776ab9208e3cb539133f9f4affd128943664
SEEWON <andyaaa78@gmail.com>
2021-12-14 19:53:34 +0900

    [Feat] Struct tab navigation

---
+++ navigation/Tabs.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useColorScheme } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  const isDark = useColorScheme() === 'dark';
+  return (
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: 'white',
+        },
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'gray',
+        headerStyle: {
+          backgroundColor: 'white',
+        },
+        headerTitleStyle: {
+          color: 'black',
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '800',
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Category"
+        component={}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <Ionicons
+                name={}
+                color={color}
+                size={size}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <Ionicons
+                name={}
+                color={color}
+                size={size}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => {
+            return (
+              <Ionicons
+                name={}
+                color={color}
+                size={size}
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
