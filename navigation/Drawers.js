b5f36beab700761e6c8594b79355271f42d20d12
SEEWON <andyaaa78@gmail.com>
2021-12-23 22:32:57 +0900

    [Fix] Fix folder name

--- navigation/Drawers.js
+++ navigation/Drawers.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../screens/Home';
-import gatherImg from '../screens/categories/gatherImg';
-import gatherLink from '../screens/categories/gatherLink';
-import gatherText from '../screens/categories/gatherText';
+import gatherImg from '../screens/DrawerNavScreens/gatherImg';
+import gatherLink from '../screens/DrawerNavScreens/gatherLink';
+import gatherText from '../screens/DrawerNavScreens/gatherText';
 
 const Drawer = createDrawerNavigator();
 
