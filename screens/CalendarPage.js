2bae17f2561c36b2efc78bd88f5540daf6c52bfd
roum02 <roum02@ewhain.net>
2021-12-30 03:11:50 +0900

    캘린더 커스텀

--- screens/CalendarPage.js
+++ screens/CalendarPage.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { LocaleConfig } from 'react-native-calendars';
+
+LocaleConfig.locales['fr'] = {
+  monthNames: [
+    'Janvier',
+    'Fevrier',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Decembre',
+  ],
+  monthNamesShort: [
+    'Janv.',
+    'Fevr.',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juil.',
+    'Août',
+    'Sept.',
+    'Oct.',
+    'Nov.',
+    'Dec.',
+  ],
+  dayNames: [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ],
+  dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+  today: "Aujourd'hui",
+};
+LocaleConfig.defaultLocale = 'fr';
 
 const CalendarPage = () => {
   const [markedDates, setMarkedDates] = React.useState(null);
   const [dates, setDates] = React.useState(['2021-12-05', '2021-12-20']);
+
   function addDates() {
     let obj = dates.reduce(
       (c, v) =>
@@ -19,13 +64,23 @@ const CalendarPage = () => {
 
   return (
     <View>
-      <Text>CalendarPage</Text>
+      <Text>캘린더</Text>
       <View>
         <Calendar
           onDayPress={(day) => {
             addDates();
           }}
           markedDates={markedDates}
+          theme={{
+            'stylesheet.calendar.header': {
+              dayTextAtIndex0: {
+                color: 'red',
+              },
+              dayTextAtIndex6: {
+                color: 'blue',
+              },
+            },
+          }}
         />
       </View>
     </View>
