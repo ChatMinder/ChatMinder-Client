7416d105605c6ed71113abdc992c4e59cc54f96d
Junhwan Yu <ujunhwan@gmail.com>
2021-12-30 23:31:53 +0900

    [feat] 카카오 소셜 로그인 기능 추가 (#1)
    * [feat] gradlew 권한 변경
    
    * [feat] @react-native-seoul/kakao-login 추가
    
    * [feat] package 추가
    
    * [feat] react-native-config package 추가
    
    * [feat] gitignore .env 추가
    
    * [feat] react-native-config 관련 설정
    
    * [feat] react-native-kakao-login 설정
    
    * [feat] 로그인 페이지

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import axios from "axios";
+import { Text, View, Button } from 'react-native';
+import {
+    KakaoOAuthToken,
+    KakaoProfile,
+    getProfile as getKakaoProfile,
+    login,
+    logout,
+    unlink, getAccessToken,
+} from '@react-native-seoul/kakao-login';
 
-const LogIn = () => {
+
+function LogIn() {
+  const [kakaoResponse, setKakaoResponse] = useState("아직 응답 받지 못하였습니다.");
+  const [serverResponse, setServerResponse] = useState("아직 응답 받지 못하였습니다.")
+  const [kakaoAccessToken, setKakaoAccessToken] = useState("아직 응답 받지 못하였습니다.");
+  const [serverAccessToken, setServerAccessToken] = useState("아직 응답 받지 못하였습니다.");
+
+  const signInWithKakao = async() => {
+      try {
+          const token = await login();
+          setKakaoResponse(JSON.stringify(token));
+          setKakaoAccessToken(token.accessToken);
+      } catch (e) {
+          console.log("error" + e)
+      }
+
+  }
+
+  const sendAccesToken = () => {
+      // const url = "http://localhost:8000/auth/kakao";
+      const url = "http://10.0.2.2:8000/auth/kakao";
+      console.log(kakaoAccessToken)
+      axios.post(url, {
+          "kakao_access_token": kakaoAccessToken
+      }).then(res => {
+          console.log(res)
+          const resdata = JSON.stringify(res.data)
+          setServerResponse(resdata)
+      }).catch(e => console.log(e))
+  }
+
   return (
     <View>
-      <Text>준환님을 위한 로그인 창</Text>
+      <Button title="Kakao Login" onPress={signInWithKakao} />
+        <Button title="Send Access Token" onPress={sendAccesToken} />
+        <Text />
+    <Text>Kakao Server response : {kakaoResponse}</Text>
+        <Text />
+        <Text>ChatMinder Server response : {serverResponse}</Text>
     </View>
   );
 };
