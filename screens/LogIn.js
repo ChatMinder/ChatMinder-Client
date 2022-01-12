62c97fd5dc8da8766b625eb927fef255664e7be5
SEEWON <andyaaa78@gmail.com>
2022-01-12 15:16:45 +0900

    [Style] 로그인 페이지 스타일링

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, Button, PixelRatio } from 'react-native';
 import { login } from '@react-native-seoul/kakao-login';
 import { PostLogIn } from '../shared/API';
+import styled from 'styled-components/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useDispatch } from 'react-redux';
 import { setLoginState } from '../shared/reducers/auth';
 import Loader from '../shared/components/Loader';
 import Logo from '../shared/assets/LoginLogo.svg';
+import LogoText from '../shared/assets/LoginText.svg';
+import KakaoSymbol from '../shared/assets/KakaoSymbol.svg';
 
-const LogIn = () => {
+import palette from '../shared/palette';
+
+const LogIn = ({ navigation }) => {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
 
@@ -37,18 +42,72 @@ const LogIn = () => {
     setLoading(false);
   };
 
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
     <>
       {loading ? (
         <Loader />
       ) : (
-        <View>
-          <Logo />
-          <Button title="카카오 로그인" onPress={loginAtOnce} />
-        </View>
+        <Wrapper>
+          <LogoContainer>
+            <Logo style={{ margin: 12 }} />
+            <LogoText />
+          </LogoContainer>
+          <LoginBtnContainer>
+            <KakaoLogIn onPress={loginAtOnce}>
+              <SymbolContainer>
+                <KakaoSymbol />
+              </SymbolContainer>
+              <Label>카카오로 시작하기</Label>
+            </KakaoLogIn>
+          </LoginBtnContainer>
+        </Wrapper>
       )}
     </>
   );
 };
+
+const Wrapper = styled.View`
+  width: 100%;
+  height: 100%;
+  background: #f7f7fa;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+const LogoContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding-top: 150px;
+  padding-bottom: 200px;
+`;
+const LoginBtnContainer = styled.View`
+  width: 100%;
+  height: 54px;
+  align-items: center;
+`;
+
+const KakaoLogIn = styled.TouchableOpacity`
+  background: #fee500;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  width: 90%;
+  height: 100%;
+`;
+const SymbolContainer = styled.View`
+  position: absolute;
+  left: 15px;
+`;
+const Label = styled.Text`
+  position: relative;
+  left: 15px;
+  font-size: 17px;
+`;
 
 export default LogIn;
