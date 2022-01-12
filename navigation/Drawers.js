4cf5963118b4c3720fd9937303e05bca90f22af7
SEEWON <andyaaa78@gmail.com>
2022-01-13 04:43:29 +0900

    [Style] Custom drawer navigation

--- navigation/Drawers.js
+++ navigation/Drawers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 import { TouchableOpacity, Text, SafeAreaView, ScrollView } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import styled, { css } from 'styled-components/native';
@@ -15,46 +16,44 @@ const Drawer = createDrawerNavigator();
 
 const MenuBox = styled.TouchableOpacity`
   background-color: ${palette.lightPurple};
+  align-self: center;
+  width: 78%;
   border-radius: 8px;
-  margin: 0 5% 5% 5%;
-  padding: 2% 0%;
+  margin-bottom: 7%;
+  padding: 6% 0%;
   justify-content: center;
   align-items: center;
   ${(props) =>
     props.marginTop &&
     css`
-      margin-top: 5%;
+      margin-top: 30%;
     `};
 `;
 
 const StyledText = styled.Text`
+  width: 80%;
   color: white;
+
   font-size: 16px;
+  font-family: 'NanumSquareOTF_ac Bold';
 `;
 
 const Drawers = () => {
+  const SCREEN_WIDTH = Dimensions.get('window').width;
   const CustomDrawerContent = (props) => (
     <SafeAreaView>
       <ScrollView>
-        <MenuBox marginTop onPress={() => props.navigation.navigate('Home')}>
-          <TextB>
-            <StyledText>Home</StyledText>
-          </TextB>
-        </MenuBox>
-        <MenuBox onPress={() => props.navigation.navigate('gatherImg')}>
-          <TextB>
-            <StyledText>이미지 모아보기</StyledText>
-          </TextB>
+        <MenuBox
+          marginTop
+          onPress={() => props.navigation.navigate('gatherImg')}
+        >
+          <StyledText>이미지 모아보기</StyledText>
         </MenuBox>
         <MenuBox onPress={() => props.navigation.navigate('gatherLink')}>
-          <TextB>
-            <StyledText>링크 모아보기</StyledText>
-          </TextB>
+          <StyledText>링크 모아보기</StyledText>
         </MenuBox>
         <MenuBox onPress={() => props.navigation.navigate('gatherText')}>
-          <TextB>
-            <StyledText>텍스트 모아보기</StyledText>
-          </TextB>
+          <StyledText>텍스트 모아보기</StyledText>
         </MenuBox>
       </ScrollView>
     </SafeAreaView>
@@ -64,6 +63,11 @@ const Drawers = () => {
     <Drawer.Navigator
       initialRouteName="Home"
       backBehavior="order"
+      screenOptions={{
+        drawerStyle: {
+          width: SCREEN_WIDTH * 0.43,
+        },
+      }}
       drawerContent={(props) => <CustomDrawerContent {...props} />}
     >
       <Drawer.Screen name="Home" component={Home} />
