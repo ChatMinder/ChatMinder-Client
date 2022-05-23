fbf25bc9d2ca96c70e042215912888860e0dfa2f
SEEWON <andyaaa78@gmail.com>
2022-05-23 17:30:41 +0900

    [Feat] configure yml file for Slack notification

--- __tests__/App.js
+++ __tests__/App.js
@@ -2,6 +2,7 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
+//test
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
